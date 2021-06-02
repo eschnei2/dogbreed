a5c0e2eee81d9ebcd9018617e819c5d8e521d283
eschnei2 <everettschneider@gmail.com>
2021-06-02 11:38:51 -0500

    chores finished, did it a little wonky

---
+++ chores.js
@@ -0,0 +1,34 @@
+const person = {
+    firstName: "Donald",
+    lastName: "McLelland"
+}
+
+const choreboy1 = boy =>
+ `${boy.firstName} ${boy.lastName} mowed the lawn`
+
+ const choreboy2 = boy =>
+ `${boy.firstName} ${boy.lastName} ate a sandwich`
+
+ const choreboy3 = boy =>
+ `${boy.firstName} ${boy.lastName} tied some shoes`
+
+ const choreboy4 = boy =>
+ `${boy.firstName} ${boy.lastName} took a nap`
+
+ const choreboy5 = boy =>
+ `${boy.firstName} ${boy.lastName} washed the dishes`
+
+ const choreboy6 = boy =>
+ `${boy.firstName} ${boy.lastName} fixed his life`
+
+ const pickChores = (first, second, third) => {
+     const pickedChores = [first, second, third]
+     return pickedChores
+ }
+ 
+const dayOne = pickChores(choreboy1(person), choreboy2(person), choreboy3(person))
+
+console.log(dayOne)
+
+
+
